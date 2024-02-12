f4ff1db3503dd50cc4407d42aaa648011c0b1e23
Marslinho <m.wendling2808@gmx.de>
2024-02-12 12:38:52 +0100

    js basics finished

---
+++ js-masterclass/00_getting_started/08_apiCall.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const http = require("http");
+const https = require("https");
+const url = "https://api.spacexdata.com/v4/launches/latest";
+
+https.get(url, res => {
+  let data = "";
+
+  res.on("data", chunk => {
+    console.log("data:", chunk);
+    data += chunk;
+  });
+
+  res.on("end", () => {
+    const dataObj = JSON.parse(data);
+    console.log("end:", dataObj.name);
+  });
+  console.log(res.statusCode);
+});
